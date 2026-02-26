53e193ae4e36aa8c3360a23d954074f09778b1e7
Dassault Rafale <23348359+rafale2k@users.noreply.github.com>
2026-02-26 20:52:41 +0900

    fix: Use BASH_SOURCE for robust script path

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,12 @@
 alias zshconfig="nano ~/dotfiles/zsh/.zshrc"
 alias reload="source ~/.zshrc"
+# Esc 2回で先頭に sudo をつける (Zsh用)
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER != sudo\ * ]]; then
+        BUFFER="sudo $BUFFER"
+        CURSOR=$#BUFFER
+    fi
+}
+zle -N sudo-command-line
+bindkey "\e\e" sudo-command-line

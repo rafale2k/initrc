d8fbb31f63c0bbacb109e1ab44dfe30c246b5470
Dassault Rafale <23348359+rafale2k@users.noreply.github.com>
2026-04-30 20:56:09 +0900

    quick update: 2026-04-30 20:56:09

--- bash/options.sh
+++ bash/options.sh
@@ -30,7 +30,6 @@ fi
 if command -v zoxide >/dev/null 2>&1; then
     eval "$(zoxide init bash --no-aliases)"
     PROMPT_COMMAND="${PROMPT_COMMAND//__zoxide_hook;/}"
-    PROMPT_COMMAND="${PROMPT_COMMAND//__zoxide_hook/}"
     alias z='__zoxide_z'
     alias zi='zi'
 fi
